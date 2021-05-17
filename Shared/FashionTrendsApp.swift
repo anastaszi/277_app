c5646ad645796be032cce8a28235fe5e4c807602
Anastasia Zimina <anastasba@gmail.com>
2021-05-16 23:07:11 -0700

    Initial Commit

---
+++ Shared/FashionTrendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FashionTrendsApp.swift
+//  Shared
+//
+//  Created by Anastasia Zimina on 5/16/21.
+//
+
+import SwiftUI
+
+@main
+struct FashionTrendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
